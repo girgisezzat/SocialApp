e7cec5d7f813f2e023b7b33c8953bd089a6694b9
girgisezzat <81932443+girgisezzat@users.noreply.github.com>
2022-04-05 14:04:43 +0200

    SocialApp

--- lib/main.dart
+++ lib/main.dart
@@ -86,6 +86,7 @@ class MyApp extends StatelessWidget {
               ..getMyPostData()
           ),
         ],
+        //SocialApp
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: lightTheme,
